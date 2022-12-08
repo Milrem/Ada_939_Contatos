0a16c9804a85b6061f73afbf09921f6a4805a32c
milrem <cris.a.neves@gmail.com>
2022-12-08 20:56:26 -0300

    Exemplo de utilização do ConsoleUIHelper

--- src/main/java/com/letscode/App.java
+++ src/main/java/com/letscode/App.java
@@ -1,39 +1,19 @@
 package com.letscode;
 
-import com.letscode.enums.TipoContato;
-import com.letscode.model.Contato;
-import com.letscode.model.Telefone;
+import com.letscode.util.ConsoleUIHelper;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Rodou!");
-
-        Contato jose = new Contato("José", "Maria");
-        jose.setNome("José");
-        jose.setSobrenome("Maria");
-        jose.setTipo(TipoContato.Pessoal);
-
-        Contato maria = new Contato("Maria", TipoContato.Profissional);
-        maria.setNome("José");
-        maria.setSobrenome("Maria");
-        maria.setTipo(TipoContato.Profissional);
-
-        Contato copia = new Contato("copia", new Telefone());
-
-
-        Integer n1 = 2;
-        Integer n2 = 2;
-
-        System.out.println("Contato José: " + jose);
-        System.out.println("Contato Maria: " + maria);
-        System.out.println("Contato Cópia: " + copia);
-        System.out.println("José == Maria:" + jose.equals(maria));
-
-        System.out.println("n1 == n2:" + n1.equals(n2));
-
-        System.out.println("|" + jose.getNomeCompleto() + "|");
-        System.out.println("|" + maria.getNomeCompleto() + "|");
-        System.out.println("|" + copia.getNomeCompleto() + "|");
-
+        int w = 80;
+        ConsoleUIHelper.drawHeader("Agenda", w);
+        ConsoleUIHelper.drawWithPadding("12345678901234567890", w);
+        ConsoleUIHelper.drawWithRightPadding("12345678901234567890", w, '_');
+        ConsoleUIHelper.drawWithRightPadding("12345678901234567890", w, '*');
+        ConsoleUIHelper.fillVSpace(5, w);
+        ConsoleUIHelper.drawWithPadding("12345678901234567890", w);
+        int corFrente = ConsoleUIHelper.askChooseOption("Escolha uma cor de frente", "Padrão", "Verde", "Amarelo", "Vermelho");
+        System.out.println("Cor escolhida: " + corFrente);
+        int corFundo = ConsoleUIHelper.askChooseOption("Escolha uma cor de fundo", "Verde", "Amarelo", "Vermelho");
+        System.out.println("Cor escolhida: " + corFundo);
     }
 }
